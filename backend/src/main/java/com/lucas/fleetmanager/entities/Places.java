15633bdc433c38c31bafb03f3d2c7ab58e7d3e5d
lfalves <lfalves@eservicios.indracompany.com>
2020-08-22 17:48:39 +0100

    Solving indentation problem in config.yml, and removing unused imports in Places.java

--- backend/src/main/java/com/lucas/fleetmanager/entities/Places.java
+++ backend/src/main/java/com/lucas/fleetmanager/entities/Places.java
@@ -2,7 +2,6 @@ package com.lucas.fleetmanager.entities;
 
 import lombok.Builder;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
